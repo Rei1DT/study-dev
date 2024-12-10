c2fbdad867aa9be1e2e09529b99f1afd568f1512
Rei1DT <reinaldo.s.santos8@aluno.senai.br>
2024-12-10 17:09:30 -0300

    added js fundamentals

---
+++ src/js/fundamentals.js
@@ -0,0 +1,25 @@
+// variáveis
+let váriavel = "sou declarável"
+const constante = "não é redeclarável"
+
+// váriavel = 1
+// constante = 2
+
+
+// console
+console.log(váriavel)
+console.error(constante)
+console.info("sou uma informação")
+console.warn("var é depreciado")
+
+//tipos de dados
+const lastname = 'santos'
+let age = 23
+const person = {
+    name: "reinaldo",
+    age: 16,
+    gender: 'male'
+}
+const fruits = ['banana' , 'uva']
+
+console.table(fruits)
